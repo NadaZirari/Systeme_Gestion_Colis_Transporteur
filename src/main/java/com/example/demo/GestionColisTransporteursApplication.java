0149645afd3d3f29ca3566b0241d08e883ef3584
Nada Zirari <zirarinada20@gmail.com>
2025-11-17 17:19:28 +0100

    initialiser la structure de projet

---
+++ src/main/java/com/example/demo/GestionColisTransporteursApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionColisTransporteursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionColisTransporteursApplication.class, args);
+	}
+
+}
