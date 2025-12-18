4d6ddfde86ebef4c2fecf8161f79c5905bbf07d5
Nada Zirari <zirarinada20@gmail.com>
2025-12-18 09:44:20 +0100

    ajouter le model du colis

--- src/main/java/com/example/demo/entity/Colis.java
+++ src/main/java/com/example/demo/entity/Colis.java
@@ -1,5 +1,38 @@
 package com.example.demo.entity;
 
+import com.example.demo.enums.ColisType;
+import com.example.demo.enums.StatutColis;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "colis")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Colis {
 
+    @Id
+    private String id;
+
+    private ColisType type;
+    private double poids;
+    private String adresseDestination;
+
+    private StatutColis statut;
+
+    // Référence transporteur
+    private String transporteurId;
+
+    // FRAGILE uniquement
+    private String instructionsManutention;
+
+    // FRIGO uniquement
+    private Double temperatureMin;
+    private Double temperatureMax;
 }
+
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -3,11 +3,17 @@ package com.example.demo.entity;
 import com.example.demo.enums.ColisType;
 import com.example.demo.enums.Role;
 import com.example.demo.enums.StatutTransporteur;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "users")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
     @Id
     private String id;
