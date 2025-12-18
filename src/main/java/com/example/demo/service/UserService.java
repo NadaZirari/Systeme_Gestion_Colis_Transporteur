521c80aa1595dfb07a45c96dd3fb6c614ed20747
Nada Zirari <zirarinada20@gmail.com>
2025-12-18 09:50:57 +0100

    ajouter serviceuser

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.User;
+import com.example.demo.enums.ColisType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface UserService {
+        User createTransporteur(User user);
+
+User updateTransporteur(String id, User user);
+
+void deleteTransporteur(String id);
+
+Page<User> getAllUsers(Pageable pageable);
+
+Page<User> getTransporteurs(Pageable pageable);
+
+Page<User> getTransporteursBySpecialite(
+        ColisType specialite,
+        Pageable pageable
+);
+
+User activateUser(String id);
+
+Optional<User> findByLogin(String login);
+}
