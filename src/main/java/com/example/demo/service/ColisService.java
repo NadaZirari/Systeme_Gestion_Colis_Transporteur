b51e142fbd89f1888d5fb29125a2ce010b338b0d
Nada Zirari <zirarinada20@gmail.com>
2025-12-18 09:52:23 +0100

    ajouter servicecolis

---
+++ src/main/java/com/example/demo/service/ColisService.java
@@ -0,0 +1,43 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Colis;
+import com.example.demo.entity.User;
+import com.example.demo.enums.ColisType;
+import com.example.demo.enums.StatutColis;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ColisService {
+        Colis createColis(Colis colis);
+
+Colis updateColis(String id, Colis colis);
+
+void deleteColis(String id);
+
+Colis assignColisToTransporteur(
+        String colisId,
+        String transporteurId
+);
+
+Colis updateStatutColis(
+        String colisId,
+        StatutColis statut,
+        User connectedUser
+);
+
+Page<Colis> getAllColis(
+        ColisType type,
+        StatutColis statut,
+        Pageable pageable
+);
+
+Page<Colis> getColisByTransporteur(
+        String transporteurId,
+        Pageable pageable
+);
+
+Page<Colis> searchByAdresse(
+        String adresse,
+        Pageable pageable
+);
+}
