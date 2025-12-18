2f32c757487bbb9de20ecf1e8f36b2273d4e0bbb
Nada Zirari <zirarinada20@gmail.com>
2025-12-18 10:13:05 +0100

    ajouter dtos of logins

---
+++ src/main/java/com/example/demo/dto/LoginRequestDTO.java
@@ -0,0 +1,13 @@
+package com.example.demo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequestDTO {
+    @NotBlank
+    private String login;
+
+    @NotBlank
+    private String password;
+}
---
+++ src/main/java/com/example/demo/dto/UserResponseDTO.java
@@ -0,0 +1,19 @@
+package com.example.demo.dto;
+
+import com.example.demo.enums.ColisType;
+import com.example.demo.enums.Role;
+import com.example.demo.enums.StatutTransporteur;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UserResponseDTO {
+
+    private String id;
+    private String login;
+    private Role role;
+    private boolean active;
+    private StatutTransporteur statut;
+    private ColisType specialite;
+}
